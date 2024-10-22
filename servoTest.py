35c494123ba91f3e4eb9e7297bf1f626fa9bd877
Henrik Larsson <k.henrik.larsson@gmail.com>
2024-10-22 22:40:30 +0200

    first working servotests

--- servoTest.py
+++ servoTest.py
@@ -1,31 +1,25 @@
-from adafruit_servokit import ServoKit
 import time
-import gpiod
+from adafruit_servokit import ServoKit
+
+camServo = 4
+leftMotor = 5
+rightMotor = 6
 
 kit = ServoKit(channels=16)
 
-kit.servo[0].set_pulse_width_range(1000, 2000)
+kit.servo[camServo].set_pulse_width_range(1000, 2500)
 
-for i in range(10):
-	kit.servo[0].fraction = i*0.1
-	time.sleep(0.5)
+kit.servo[camServo].fraction = 0.90
+for i in range(8):
+    val = 0.90 + (0.01 * i)
+    print("to servo: ", val)
+    kit.servo[camServo].fraction = val
+    time.sleep(2)
 
-chip = gpiod.Chip('gpiochip4', gpiod.Chip.OPEN_BY_NAME)
-LED0 = chip.get_line(5)
-LED0.request(consumer="blinktest", type=gpiod.LINE_REQ_DIR_OUT)
-LED1 = chip.get_line(6)
-LED1.request(consumer="blinktest", type=gpiod.LINE_REQ_DIR_OUT)
-LED2 = chip.get_line(13)
-LED2.request(consumer="blinktest", type=gpiod.LINE_REQ_DIR_OUT)
+kit.servo[leftMotor].set_pulse_width_range(1000, 2000)
+kit.servo[leftMotor].fraction = 0.5
 
-LED0.set_value(1)
-LED1.set_value(1)
-LED2.set_value(1)
-time.sleep(1.5)
-LED0.set_value(0)
-time.sleep(1.5)
-LED1.set_value(0)
-time.sleep(1.5)
-LED2.set_value(0)
+kit.servo[rightMotor].set_pulse_width_range(1000, 2000)
+kit.servo[rightMotor].fraction = 0.5
 
-print("worked")
+print("it worked")
